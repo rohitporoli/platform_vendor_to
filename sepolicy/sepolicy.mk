fa89164294adf1cc124a3c6a29313e833acdbd6a
William Roberts <wroberts@tresys.com>
2013-08-15 13:08:41 -0700

    sepolicy: Start Team OctOs Common sepolicy
    Rather than having to maintain out of tree changes, it is often
    easier to maintain a hiearchy of changes, starting with the vendors
    common config file. From there, inheriting products can pick up a base
    and start to add or remove certain bits from it, making use of the
    BOARD_SEPOLICY_* functions documented in external/sepolicy/README.
    
    Change-Id: I42ebb99e14d7e42ec163a79b7b1e1d0a269022ea

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,10 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Team OctOs
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/to/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+    mac_permissions.xml
