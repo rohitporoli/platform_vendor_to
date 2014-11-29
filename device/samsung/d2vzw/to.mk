e06d9617256e0a6ed2efe3d831852f6666e386ae
hedwig34 <hedwig34@gmail.com>
2014-11-29 16:04:30 -0500

    device: add d2vzw

---
+++ device/samsung/d2vzw/to.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/to/config/nfc_enhanced.mk)
+
+# Inherit some common TO stuff.
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2vzw \
+    TARGET_DEVICE=d2vzw \
+    BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.4.2/KOT49H/I535VRUDNE1:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2vzw-user 4.4.2 KOT49H I535VRUDNE1 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
+PRODUCT_NAME := to_d2vzw
+PRODUCT_DEVICE := d2vzw
