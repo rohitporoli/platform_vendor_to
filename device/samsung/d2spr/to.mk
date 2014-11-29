73161d7f7dd85791ef302fee8c1773bad1116721
hedwig34 <hedwig34@gmail.com>
2014-11-29 13:53:13 -0500

    device: add d2spr

---
+++ device/samsung/d2spr/to.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/to/config/nfc_enhanced.mk)
+
+# Inherit some common to stuff.
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2spr \
+    TARGET_DEVICE=d2spr \
+    BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.4.2/KOT49H/L710VPUDND8:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2spr-user 4.4.2 KOT49H L710VPUDND8 release-keys"
+
+PRODUCT_NAME := to_d2spr
+PRODUCT_DEVICE := d2spr
+
