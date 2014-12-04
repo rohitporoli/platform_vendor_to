b03f205ab2063cef6cb1717098a631cceda6168e
hedwig34 <hedwig34@gmail.com>
2014-12-04 09:44:04 -0500

    jflteatt: Add jflteatt
    Change-Id: I54f8c56f11ab2cf23c80221aa5c5611e71b297ff

---
+++ device/samsung/jflteatt/to.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/to/config/nfc_enhanced.mk)
+
+# Inherit some common TO stuff.
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jflteuc \
+  TARGET_DEVICE=jflteatt \
+  BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.4.4/KTU84P/I337UCUFNJ4:user/release-keys" \
+  PRIVATE_BUILD_DESC="jflteuc-user 4.4.4 KTU84P I337UCUFNJ4 release-keys"
+
+PRODUCT_NAME := to_jflteatt
+PRODUCT_DEVICE := jflteatt
