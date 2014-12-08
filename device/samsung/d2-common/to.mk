f9d6b7db3dac5232e0081f2dbd28a2251a0f0f19
hedwig34 <hedwig34@gmail.com>
2014-12-08 10:37:13 -0500

    d2-common: Override the inherited kernel boot splash with a generic one.
    Change-Id: Ifb1cac5345c5f11bbb9b18c9a6f311d282c56379

--- device/samsung/d2-common/to.mk
+++ device/samsung/d2-common/to.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Remove inilogo.rle from PRODUCT_COPY_FILES
+TARGET_COPY_FILES_OVERRIDES := \
+    root/initlogo.rle
+
+# Add initlogo.rle as a module
+PRODUCT_PACKAGES += \
+    initlogo.rle
+
 # TWRP2 Support
 RECOVERY_VARIANT := twrp
 DEVICE_RESOLUTION := 720x1280
