ee2e0bceb2bc1992d185ed79b2af93f1fc01daa4
hedwig34 <hedwig34@gmail.com>
2014-12-10 19:02:31 -0500

    i9300: Add i9300
    Change-Id: I49dae4b1e75700c9673279c03c13ad8cb86c6694

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo to_d2vzw-userdebug
 add_lunch_combo to_flo-userdebug
 add_lunch_combo to_grouper-userdebug
 add_lunch_combo to_hammerhead-userdebug
+add_lunch_combo to_i9300-userdebug
 add_lunch_combo to_jflteatt-userdebug
 add_lunch_combo to_jfltespr-userdebug
 add_lunch_combo to_jfltetmo-userdebug
