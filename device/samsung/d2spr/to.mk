426f2890028939070ad437e9910502a14407c1d7
hedwig34 <hedwig34@gmail.com>
2014-12-15 15:55:44 -0500

    d2spr: Start tracking our own d2spr
    Change-Id: I1ca822c930a8a20791d339a9155a43307793a3aa

--- device/samsung/d2spr/to.mk
+++ device/samsung/d2spr/to.mk
@@ -15,8 +15,6 @@ $(call inherit-product, vendor/to/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
     TARGET_DEVICE=d2spr \
-    BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.4.2/KOT49H/L710VPUDND8:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2spr-user 4.4.2 KOT49H L710VPUDND8 release-keys"
 
 PRODUCT_NAME := to_d2spr
 PRODUCT_DEVICE := d2spr
