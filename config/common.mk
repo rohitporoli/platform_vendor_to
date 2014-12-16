2b1bb523442068fbbf2c3a51e2d973ed4c3b7ebe
Arne Coucheron <arco68@gmail.com>
2014-12-17 00:51:55 +0100

    cm: Remove obsolete persist.sys.dalvik.multithread property
    Change-Id: I0d04f50dcca1454a7f38d8076b8418f67b140906

--- config/common.mk
+++ config/common.mk
@@ -22,10 +22,6 @@ include vendor/to/config/googleprops.mk
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
-# Disable multithreaded dexopt by default
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.dalvik.multithread=false
-
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
