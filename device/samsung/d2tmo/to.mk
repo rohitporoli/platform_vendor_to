c12e78b44b4d69e0518fe873c1f574084d6e79d5
hedwig34 <hedwig34@gmail.com>
2014-12-20 14:22:06 -0500

    d2tmo: updated fingerprint
    Change-Id: I764cc8d677d658c0157906f8a7097ea204831f96

--- device/samsung/d2tmo/to.mk
+++ device/samsung/d2tmo/to.mk
@@ -15,8 +15,8 @@ $(call inherit-product, vendor/to/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2tmo \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.1.2/JZO54K/T999UVDMD5:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user 4.1.2 JZO54K T999UVDMD5 release-keys"
+    BUILD_FINGERPRINT="samsung/d2uc/d2tmo:4.3/JSS15J/T999UVUENC2:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2uc-user 4.3 JSS15J T999UVUENC2 release-keys"
 
 PRODUCT_NAME := to_d2tmo
 PRODUCT_DEVICE := d2tmo
