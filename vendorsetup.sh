d9c1fd94e73db42731e8a7c242cf69077eedfa13
Don <cjkacz@gmail.com>
2014-12-30 15:16:36 -0500

    Add d851 (G3 tmo)
    Change-Id: I254143d9c4afc3da8230b8fe94bbbd44d5ad2266

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,6 +13,7 @@ add_lunch_combo to_jfltetmo-userdebug
 add_lunch_combo to_jflteusc-userdebug
 add_lunch_combo to_jfltevzw-userdebug
 add_lunch_combo to_jfltexx-userdebug
+add_lunch_combo to_d851-userdebug
 add_lunch_combo to_ls990-userdebug
 add_lunch_combo to_vs985-userdebug
 add_lunch_combo to_mako-userdebug
