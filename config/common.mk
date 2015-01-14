c054d2a67f65efb072f18d08715bc121e91ca483
Tom Marshall <tdm@cyngn.com>
2015-01-14 08:40:53 -0500

    cm: Put install tools in OUT/install
     * This allows us to avoid modifying /system at install time.
    
    Conflicts:
    	config/common.mk
    
    Change-Id: I73ca4952d849b265681425f60e66e863c5d85580

--- config/common.mk
+++ config/common.mk
@@ -39,15 +39,15 @@ endif
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
-    vendor/to/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/to/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/to/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/to/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/to/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh \
     vendor/to/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 endif
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
-    vendor/to/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
+    vendor/to/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
 
 # init.d support
 PRODUCT_COPY_FILES += \
