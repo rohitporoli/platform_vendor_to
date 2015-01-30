e73bde91e79120b57b53210ca681820eb6413736
hedwig34 <hedwig34@gmail.com>
2015-01-29 20:14:24 -0500

    t0ltetmo: Add Galaxy Note 2 (T-Mobile)
    Change-Id: Ie3196b7db25f6d74cd7389993862016fbe279836

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,7 @@ add_lunch_combo to_ls990-userdebug
 add_lunch_combo to_vs985-userdebug
 add_lunch_combo to_mako-userdebug
 add_lunch_combo to_shamu-userdebug
+add_lunch_combo to_t0ltetmo-userdebug
 add_lunch_combo to_t6vzw-userdebug
 add_lunch_combo to_m8-userdebug
 add_lunch_combo to_targa-userdebug
