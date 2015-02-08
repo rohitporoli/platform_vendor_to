a693146437b48f57ab78acaad74c1caed9656cb1
hedwig34 <hedwig34@gmail.com>
2015-02-08 10:15:47 -0500

    t0lte: Add libs to fix GPS on Lollipop
    Change-Id: I62cf8d3bec2e7649b0b80c91464e9577471c6672

--- device/samsung/t0lte/to.mk
+++ device/samsung/t0lte/to.mk
@@ -1,2 +1,10 @@
 # t0lte over-ride
 TARGET_POWERHAL_VARIANT := cm
+
+# GPS fix
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/lib/libgps.utils.so:system/lib/libgps.utils.so \
+    $(LOCAL_PATH)/rootdir/lib/libloc_adapter.so:system/lib/libloc_adapter.so \
+    $(LOCAL_PATH)/rootdir/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
+    $(LOCAL_PATH)/rootdir/lib/libloc_eng.so:system/lib/libloc_eng.so \
+    $(LOCAL_PATH)/rootdir/lib/hw/gps.default.so:system/lib/hw/gps.default.so
