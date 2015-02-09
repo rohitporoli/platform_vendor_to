9b387c0b8ceffa6155df7582eb9666a5ee395408
hedwig34 <hedwig34@gmail.com>
2015-02-09 11:04:21 -0500

    d2-common: use sabermod toolchains
    Change-Id: Idaa43983a3eca8053c74f03221a9e5567721855b

--- device/samsung/d2-common/to.mk
+++ device/samsung/d2-common/to.mk
@@ -1,6 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Custom toolchains
+TARGET_GCC_VERSION_AND := 4.8-sm
+TARGET_GCC_VERSION_ARM := 4.9-sm
+
 # Remove inilogo.rle from PRODUCT_COPY_FILES
 TARGET_COPY_FILES_OVERRIDES := \
     root/initlogo.rle
