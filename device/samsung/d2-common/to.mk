782d3522fa73bded50f8b1517d738c89041268fb
hedwig34 <hedwig34@gmail.com>
2015-02-10 15:34:34 -0500

    d2-common: Enable LIQUIFY OPTIMIZATIONS V1
    Change-Id: Id4448af220ec620e6977c87d5dc5826a77f5dd8a

--- device/samsung/d2-common/to.mk
+++ device/samsung/d2-common/to.mk
@@ -5,6 +5,9 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 TARGET_GCC_VERSION_AND := 4.8-sm
 TARGET_GCC_VERSION_ARM := 4.9-sm
 
+#Enable LIQUIFY OPTIMIZATIONS V1
+LIQUIFY := TRUE
+
 # Remove inilogo.rle from PRODUCT_COPY_FILES
 TARGET_COPY_FILES_OVERRIDES := \
     root/initlogo.rle
