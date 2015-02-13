fc857547f52baf0820da96b64fadc536c3c12352
hedwig34 <hedwig34@gmail.com>
2015-02-13 14:03:39 -0500

    t0lteatt: Add support for t0lteatt (SGH-I317)           Note 2 AT&T
    Change-Id: I57b1a8f72a2b71362e3e9a14e49b25de9b40cc55

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,6 +21,7 @@ add_lunch_combo to_vs985-userdebug
 add_lunch_combo to_mako-userdebug
 add_lunch_combo to_shamu-userdebug
 add_lunch_combo to_t0lte-userdebug
+add_lunch_combo to_t0lteatt-userdebug
 add_lunch_combo to_t0ltetmo-userdebug
 add_lunch_combo to_t6vzw-userdebug
 add_lunch_combo to_m8-userdebug
