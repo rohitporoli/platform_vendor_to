5f2e9d22557e4ac8fd25e2318154a6e22a69b3e6
Donald Hoskins <grommish@gmail.com>
2015-02-21 19:33:05 -0500

    Vendor: Explicity Set LIQUIFY to FALSE, remove SaberMOD toolchain Defines.
    SaberMOD is now considered "Closed Source".  At this time, we
    are unsure if we wish to continue using them.
    
    Change-Id: I1a5aede31a0d94133ee997b23fc80f0d590c2405

--- device/lge/g3-common/to.mk
+++ device/lge/g3-common/to.mk
@@ -2,7 +2,7 @@
 TARGET_POWERHAL_VARIANT := cm
 
 # Custom toolchains
-override TARGET_GCC_VERSION_AND := 4.8-sm
-override TARGET_GCC_VERSION_ARM := 4.9-sm
+# override TARGET_GCC_VERSION_AND := 4.8-sm
+# override TARGET_GCC_VERSION_ARM := 4.9-sm
 
-LIQUIFY := TRUE
+LIQUIFY := FALSE
--- device/samsung/jf-common/to.mk
+++ device/samsung/jf-common/to.mk
@@ -1,7 +1,7 @@
 # jf-common over-ride
 TARGET_POWERHAL_VARIANT := cm
-LIQUIFY := TRUE
+# LIQUIFY := FALSE
 
 # Custom toolchains
-override TARGET_GCC_VERSION_AND := 4.8-sm
-override TARGET_GCC_VERSION_ARM := 4.9-sm
+# override TARGET_GCC_VERSION_AND := 4.8-sm
+# override TARGET_GCC_VERSION_ARM := 4.9-sm
