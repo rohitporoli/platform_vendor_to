4dd5a701334005af2babb8168c4f262b361e778e
hedwig34 <hedwig34@gmail.com>
2015-02-26 16:12:30 -0500

    Include OctInkDark theme in the build     but install it to /data
    Change-Id: If774aa1fc043d5b54e97a18963a337cba98406d0

--- config/common.mk
+++ config/common.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
     OctPhase \
     OctSun
 
+# Team-OctOS themes
+PRODUCT_PACKAGES += \
+    OctInkDark
+
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
