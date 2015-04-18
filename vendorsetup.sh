4c53ef4760794afe1318cffb73d0fd5c01918d9c
Donald Hoskins <grommish@gmail.com>
2015-04-18 11:04:24 -0400

    Bring up MotoX (Ghost)
    Change-Id: I233999049e60bc0eec2033a8827695e234299797

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,6 +6,7 @@ add_lunch_combo to_d2vzw-userdebug
 add_lunch_combo to_d850-userdebug
 add_lunch_combo to_d851-userdebug
 add_lunch_combo to_flo-userdebug
+add_lunch_combo to_ghost-userdebug
 add_lunch_combo to_grouper-userdebug
 add_lunch_combo to_hammerhead-userdebug
 add_lunch_combo to_hlte-userdebug
