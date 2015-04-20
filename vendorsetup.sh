2022e83f363bfd320f21b3d139db63ab085d051c
hedwig34 <hedwig34@gmail.com>
2015-04-20 10:39:15 -0400

    vendorsetup, fix the order :P
    Change-Id: I2ee3d25f6919c03891f9087e9afc4bbf914b28b8

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 add_lunch_combo to_d2att-userdebug
 add_lunch_combo to_d2bst-userdebug
 add_lunch_combo to_d2spr-userdebug
-add_lunch_combo to_d2vzw-userdebug
 add_lunch_combo to_d2tmo-userdebug
+add_lunch_combo to_d2vzw-userdebug
 add_lunch_combo to_d850-userdebug
 add_lunch_combo to_d851-userdebug
 add_lunch_combo to_flo-userdebug
