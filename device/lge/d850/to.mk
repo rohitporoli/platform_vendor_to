949a9a73ac7ee2ee2fa920a2e9504128e7e54f82
Donald Hoskins <grommish@gmail.com>
2015-04-20 17:44:04 -0400

    LG G3 D850: Correct Fingerprint for A5.1
    Change-Id: I204e5f259fa673f9e7605c61261d77430a6ad9da

--- device/lge/d850/to.mk
+++ device/lge/d850/to.mk
@@ -13,5 +13,5 @@ PRODUCT_NAME := to_d850
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_att_us" \
-    BUILD_FINGERPRINT="lge/g3_att_us/g3:4.4.2/KVT49L.D85010d/D85010d.1403173065:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_att_us-user 4.4.2 KVT49L.D85010d D85010d.1403173065 release-keys"
+    BUILD_FINGERPRINT="lge/g3_att_us/g3:5.0.1/LRX21Y/D85020f.1421382354:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_att_us-user 5.0.1 LRX21Y D85020f.1421382354 release-keys"
