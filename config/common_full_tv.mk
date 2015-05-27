d793c3d9a514a1b929c314e0d107715d73a24e16
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-05-27 01:27:08 -0400

    config: Create TV targets (full and mini)
    * To follow naming and allow proper device type makefile inclusion
         (phone calls _phone, tablet calls _tablet, etc.)
    
    Change-Id: Ifb42fdbaf7368d17f22905b3ed99feeb3c9c9e74

---
+++ config/common_full_tv.mk
@@ -0,0 +1,12 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/to/config/common_full.mk)
+
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Cesium.ogg
+
+ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    PRODUCT_COPY_FILES += \
+        vendor/to/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+endif
---
+++ config/common_mini_tv.mk
@@ -0,0 +1,15 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/to/config/common.mk)
+
+# Include CM audio files
+#include vendor/to/config/audio.mk
+
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Cesium.ogg
+
+ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+    PRODUCT_COPY_FILES += \
+        vendor/to/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+endif
