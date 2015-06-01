12efc1fdc4502071d27c45b59e7e7a2b9b307e05
hedwig34 <hedwig34@gmail.com>
2015-06-01 15:44:56 -0400

    g3-common: Add obverlays for the AboutOctOs app to display the device maintainter's info.
    Change-Id: Id3cc00d07b62752016659cbe4d66f2e57d5b1953

--- device/lge/g3-common/to.mk
+++ device/lge/g3-common/to.mk
@@ -1,6 +1,11 @@
 # g3-common over-ride
 TARGET_POWERHAL_VARIANT := cm
 
+# Team OctOs device maintainer info
+ifneq (COMMUNITY, $(TO_BUILDTYPE))
+DEVICE_PACKAGE_OVERLAYS += vendor/to/device/lge/g3-common/maintainer
+endif
+
 # Custom toolchains
 # override TARGET_GCC_VERSION_AND := 4.8-sm
 # override TARGET_GCC_VERSION_ARM := 4.9-sm
