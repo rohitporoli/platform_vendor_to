84c810fe52d32df4c1512aa2f10d87baed66344e
hedwig34 <hedwig34@gmail.com>
2015-06-01 15:08:23 -0400

    d2-common: Add obverlays for the AboutOctOs app to display the device maintainter's info.
    Change-Id: Iaf26a766c1d09e22307040c6a48024ad6dae4ad8

--- device/samsung/d2-common/to.mk
+++ device/samsung/d2-common/to.mk
@@ -1,6 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Team OctOs device maintainer info
+ifneq (COMMUNITY, $(TO_BUILDTYPE))
+DEVICE_PACKAGE_OVERLAYS += vendor/to/device/samsung/d2-common/maintainer
+endif
+
 # Custom toolchains
 # TARGET_GCC_VERSION_AND := 4.8-sm
 # TARGET_GCC_VERSION_ARM := 4.9-sm
