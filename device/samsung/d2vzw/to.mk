c7d5e1a060f0a7e2fc5f57f8045bda82bf2b5a62
hedwig34 <hedwig34@gmail.com>
2015-07-20 12:31:23 -0400

    How'd this get in here... oops
    Change-Id: I9150eacee54edc9518f92c4f6c40c6707405f6e3

--- device/samsung/d2vzw/to.mk
+++ device/samsung/d2vzw/to.mk
@@ -1,8 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-override KERNEL_DECONFIG := octos_d2_defconfig
-
 $(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
 # Inherit device common stuff specific to TO
