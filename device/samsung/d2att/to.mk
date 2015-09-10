17af5a29aa1c3b6aa4109a1191abb5c13e1ed86e
Don Kaczmarek <cjkacz@gmail.com>
2015-09-09 21:48:39 -0400

    d2att fix flash update binary
    Change-Id: I55c4f942cc0e60f24ba0c2c761903e852ee19eb4

--- device/samsung/d2att/to.mk
+++ device/samsung/d2att/to.mk
@@ -13,10 +13,10 @@ $(call inherit-product, vendor/to/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/to/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=d2uc \
-    TARGET_DEVICE=d2att \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNE4:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNE4 release-keys"
+   PRODUCT_NAME=d2uc \
+   TARGET_DEVICE=d2att \
+   BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNJ1:user/release-keys" \
+   PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNJ1 release-keys"
 
 PRODUCT_NAME := to_d2att
 PRODUCT_DEVICE := d2att
