44070ec46c830c27bcf5159bea77005bad800a6c
hedwig34 <hedwig34@gmail.com>
2015-09-13 11:55:08 -0400

    d2tmo: Updated T999 baseband fingerprint to latest availabl
    Change-Id: Iba007203a637e3b389d3843181a2628ecd258321

--- device/samsung/d2tmo/to.mk
+++ device/samsung/d2tmo/to.mk
@@ -15,8 +15,8 @@ $(call inherit-product, vendor/to/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2tmo \
-    BUILD_FINGERPRINT="samsung/d2uc/d2tmo:4.3/JSS15J/T999UVUENC2:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user 4.3 JSS15J T999UVUENC2 release-keys"
+    BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.3/JSS15J/T999UVUEMJC:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2tmo-user 4.3 JSS15J T999UVUEMJC release-keys"
 
 PRODUCT_NAME := to_d2tmo
 PRODUCT_DEVICE := d2tmo
