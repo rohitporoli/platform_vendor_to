9574e1a08f24f12a6d440ee1bcf6c1a6a7a39475
hedwig34 <hedwig34@gmail.com>
2015-10-14 10:21:31 -0400

    vendor: Add shamu, hammerhead, and flo
    Change-Id: Idde06bc9b58f74eb3b651f0450bcc523009fe917

---
+++ product/lge/hammerhead/to.mk
@@ -0,0 +1,32 @@
+# Copyright (C) 2015 Team OctOs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit some common Team OctOs configuration
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+# Inherit AOSP device configuration for hammerhead
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := to_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := hammerhead
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=hammerhead \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0/MRA58K/2256973:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0 MRA58K 2256973 release-keys"
