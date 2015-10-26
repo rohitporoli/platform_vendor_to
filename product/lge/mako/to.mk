d30dd90c018ca521d458c939a2c56ffc5a97741d
richard <rcovington79@gmail.com>
2015-10-26 10:04:16 -0400

    bring up mako
    Change-Id: I73587587cd8688e4c75f717bcc0cb011db44783b

---
+++ product/lge/mako/to.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2015 Team OctOs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
+# Inherit some common Team OctOs configuration
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+# Inherit AOSP device configuration for mako
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/to/config/nfc_enhanced.mk)
+
+# Setup device specific product configuration
+PRODUCT_NAME := to_mako
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := mako
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48M/2167285:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48M 2167285 release-keys"
