361ed37596ba62b06aefccd6998f76776607c6af
Don Kaczmarek <cjkacz@gmail.com>
2015-12-24 00:03:04 -0500

    bring up ghost
    Change-Id: Ibaa853562938161bc48e3474cd9859834b1bfecb

---
+++ product/moto/ghost/to.mk
@@ -0,0 +1,22 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/to/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/ghost/full_ghost.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ghost
+PRODUCT_NAME := to_ghost 
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := ghost 
+PRODUCT_MANUFACTURER := motorola
+# Enable Torch
+PRODUCT_PACKAGES += Torch
+TARGET_POWERHAL_VARIANT := cm
