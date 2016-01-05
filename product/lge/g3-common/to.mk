012e16d3b3bb2d496643745d425a5513aa14e1c1
hedwig34 <hedwig34@gmail.com>
2016-01-05 11:22:44 -0500

    g3-common: Remove unnecessary mkdir :P
    Change-Id: I5c998bb0e5e3fc61a63b8d28370908831d8f71a8

--- product/lge/g3-common/to.mk
+++ product/lge/g3-common/to.mk
@@ -9,5 +9,4 @@ endif
 override TARGET_RELEASETOOLS_EXTENSIONS := vendor/to/product/lge/g3-common
 
 # Copy bump files to $OUT
-$(shell mkdir -p $(OUT))
 $(shell cp -r $(ANDROID_BUILD_TOP)/vendor/to/product/lge/g3-common/octotools $(OUT))
