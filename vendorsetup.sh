992e024440b3e9b17e247e2a34db7621b6bdce10
Don <cjkacz@gmail.com>
2016-01-14 16:06:14 -0500

    Add Grouper
      patchset #5 switch to Team OctOs repos
    
    Change-Id: Ib4eb85313b08968e56f1b09790ade0ded89e3ca5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,6 +10,7 @@ add_lunch_combo to_d851-userdebug
 add_lunch_combo to_d855-userdebug
 add_lunch_combo to_flo-userdebug
 add_lunch_combo to_ghost-userdebug
+add_lunch_combo to_grouper-userdebug
 add_lunch_combo to_hammerhead-userdebug
 add_lunch_combo to_jflteatt-userdebug
 add_lunch_combo to_jfltespr-userdebug
