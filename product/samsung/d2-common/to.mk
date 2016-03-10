562a4116536792ef85a8fb7ab1a479fc16ca3990
hedwig34 <hedwig34@gmail.com>
2016-03-09 20:17:23 -0500

    d2-common: Build using sabermod toolchain
    Change-Id: I61be5d0e8ae47980c014bd44b62d35a46d6dac23

--- product/samsung/d2-common/to.mk
+++ product/samsung/d2-common/to.mk
@@ -1,6 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Build using sabermod toolchain on linux but use default toolchain on darwin.
+ifneq ($(HOST_OS),darwin)
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9-sm/bin
+TARGET_GCC_VERSION_EXP := 4.9-sm
+endif
+
 # Team OctOs device maintainer info
 ifneq (COMMUNITY, $(TO_BUILDTYPE))
 DEVICE_PACKAGE_OVERLAYS += vendor/to/product/samsung/d2-common/maintainer
