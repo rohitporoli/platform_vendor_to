a344581776a91701f4d63deedafd3e0bfff87c34
hedwig34 <hedwig34@gmail.com>
2016-03-10 10:39:20 -0500

    shamu: Build using Uber toolchains.
    Change-Id: I0f86e3ffdeaf260528a71bcf5ed3df177b895676

--- product/moto/shamu/to.mk
+++ product/moto/shamu/to.mk
@@ -1,6 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Build using uber toolchain on linux but use default toolchain on darwin.
+ifneq ($(HOST_OS),darwin)
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8-uber/bin
+TARGET_GCC_VERSION_EXP := 4.9-uber
+endif
+
 # Team OctOs device maintainer info
 ifneq (COMMUNITY, $(TO_BUILDTYPE))
 DEVICE_PACKAGE_OVERLAYS += vendor/to/product/moto/shamu/maintainer
