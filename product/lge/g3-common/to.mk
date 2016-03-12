a5dd0b292dd7e616ef4effd2c77f60f8bf061caa
richard covington <rcovington79@gmail.com>
2016-03-12 11:11:55 -0500

    G3: build using Uber
    Change-Id: I21002e06021c485927dc1a89aba91d18eec4a46f

--- product/lge/g3-common/to.mk
+++ product/lge/g3-common/to.mk
@@ -1,6 +1,12 @@
 # g3-common over-ride
 TARGET_POWERHAL_VARIANT := cm
 
+# Build using uber toolchain on linux but use default toolchain on darwin.
+ifneq ($(HOST_OS),darwin)
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9-uber/bin
+TARGET_GCC_VERSION_EXP := 5.3-uber
+endif
+
 # Team OctOs device maintainer info
 ifneq (COMMUNITY, $(TO_BUILDTYPE))
 DEVICE_PACKAGE_OVERLAYS += vendor/product/lge/g3-common/maintainer
